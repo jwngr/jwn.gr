6eb0116a2a3f9f130c7374939fda4eb9f4ac58c4
Jacob Wenger <wenger.jacob@gmail.com>
2024-09-05 21:53:53 -0700

    Updated patterns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,23 @@ export function assertNever(x: never): never {
   throw new Error(`Unexpected object: ${x}`);
 }
 
+export function random(minOrMax: number, max?: number): number {
+  let actualMin: number;
+  let actualMax: number;
+  if (max === undefined) {
+    actualMin = 0;
+    actualMax = minOrMax;
+  } else {
+    actualMin = minOrMax;
+    actualMax = max;
+  }
+  return Math.floor(Math.random() * (actualMax - actualMin + 1)) + actualMin;
+}
+
+export function sample<T>(arr: T[]): T {
+  return arr[random(arr.length - 1)];
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function throttle<T extends (...args: any[]) => any>(
   func: T,
